0d64fd5198df3b22e3c7e7f3da4803942615d96d
michmauzer004 <michmauzer004@gmail.com>
2016-06-13 23:25:49 +0300

    initial

--- src/Inheritance/Sub.java
+++ src/Inheritance/Sub.java
@@ -12,7 +12,9 @@ public class Sub extends Base {
 	public static void main(String[] args) {
 		//first thing that the Sub class’s constructor does
 		// is invoke the Base class’s no-arg constructor
-		Sub s = new Sub("Start");
+		Sub s1 = new Sub("Start");
+		Sub s2;
+
 	}
 }
 class Base {
@@ -23,4 +25,6 @@ class Base {
 	public Base(String s) {
 		System.out.println("Base - " + s);
 	}
+
+
 }
