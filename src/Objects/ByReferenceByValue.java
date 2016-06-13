0fdebb5074fa5dc212c42ba1b5026ff48b6a9ed2
michmauzer004 <michmauzer004@gmail.com>
2016-06-13 23:21:20 +0300

    initial

--- src/Objects/ByReferenceByValue.java
+++ src/Objects/ByReferenceByValue.java
@@ -30,3 +30,12 @@ public class ByReferenceByValue {
 		System.out.println(p.x); // prints 0
 	}
 }
+
+//	Now pay attention to the main method. We passed a (whose value is 9)
+//	to the increment method. After the method invocation, we printed the value of a and you get 9,
+//	which means that the value of a did not change.
+//
+//	Afterwards, you create a Point object and assign the reference to p.
+//	You then initialize its fields and pass it to the reset method.
+//	The changes in the reset method affects the Point object because objects are passed by reference.
+//	As a result, when you print the value of p.x, you get 0.
