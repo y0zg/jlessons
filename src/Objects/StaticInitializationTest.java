b31fac292dc0e50c40b58c884a49857d0387b470
michmauzer004 <michmauzer004@gmail.com>
2016-06-13 23:25:37 +0300

    initial

---
+++ src/Objects/StaticInitializationTest.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class StaticInitializationTest {
+	public static int a = 5;
+	public static int b = a * 2;
+	static {
+		System.out.println("static");
+		System.out.println(b);
+	}
+	public static void main(String[] args) {
+		System.out.println("main method");
+	}
+}
