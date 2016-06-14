f11e81f77770a5e530a34e521fba87aba82fa19c
michmauzer004 <michmauzer004@gmail.com>
2016-06-14 23:00:10 +0300

    initial

--- src/Strings/CompareString.java
+++ src/Strings/CompareString.java
@@ -3,6 +3,11 @@ package Strings;
 
 public class CompareString {
 	public static void main(String[] args) {
+
+		String s3="1000";
+		int num2=Integer.parseInt(s3);
+		System.out.println(s3);
+
 		String s1 = "Java";
 		String s2 = new String("Java");
 		if (s1.equals(s2)) {
@@ -16,5 +21,7 @@ public class CompareString {
 		} else {
 			System.out.println("s1<>s2");
 		}
+
+
 	}
 }
