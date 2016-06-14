185053655cfdfa789bf97c0c67497b6659e5c367
michmauzer004 <michmauzer004@gmail.com>
2016-06-14 22:28:01 +0300

    initial

---
+++ src/Strings/CompareString.java
@@ -0,0 +1,20 @@
+package Strings;
+
+
+public class CompareString {
+	public static void main(String[] args) {
+		String s1 = "Java";
+		String s2 = new String("Java");
+		if (s1.equals(s2)) {
+			System.out.println("s1=s2");
+		} else {
+			System.out.println("s1<>s2");
+		}
+
+		if(s1==s2){
+			System.out.println("s1=s2");
+		} else {
+			System.out.println("s1<>s2");
+		}
+	}
+}
