e134e8d2baecd329760a789b3d16750d79a6f5b1
michmauzer004 <michmauzer004@gmail.com>
2016-06-14 22:32:41 +0300

    initial

--- src/Strings/CompareString.java
+++ src/Strings/CompareString.java
@@ -6,7 +6,7 @@ public class CompareString {
 		String s1 = "Java";
 		String s2 = new String("Java");
 		if (s1.equals(s2)) {
-			System.out.println("s1=s2");
+			System.out.println("\ts1=s2 ");
 		} else {
 			System.out.println("s1<>s2");
 		}
@@ -18,3 +18,14 @@ public class CompareString {
 		}
 	}
 }
+//	Here are some escape sequences:
+//
+//		\u          /* a Unicode character
+//       \b          /* \u0008: backspace BS */
+//		\t          /* \u0009: horizontal tab HT */
+//		\n          /* \u000a: linefeed LF */
+//		\f          /* \u000c: form feed FF */
+//		\r          /* \u000d: carriage return CR */
+//		\"          /* \u0022: double quote " */
+//		\'          /* \u0027: single quote ' */
+//		\\          /* \u005c: backslash \ */
