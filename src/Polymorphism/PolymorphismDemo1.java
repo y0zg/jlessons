dc2257469f4365e3c9d8bc904c0a99e3af19e1bc
michmauzer004 <michmauzer004@gmail.com>
2016-06-16 23:16:50 +0300

    initial

--- src/Polymorphism/PolymorphismDemo1.java
+++ src/Polymorphism/PolymorphismDemo1.java
@@ -26,5 +26,12 @@ public class PolymorphismDemo1 {
 // java: incompatible types: Polymorphism.Employee cannot be converted to Polymorphism .Manager
 		Manager man= (Manager) emp;
 		man.manage(); //managing...
+
+/*After seeing the code, you might ask, why would you declare employee as Employee
+in the first place? Why didn’t you declare employee as type Manager, such as this?
+Manager employee;
+employee = new Manager();
+You do this to ensure flexibility in cases where you don’t know whether the
+object reference (employee) will be assigned an instance of Manager or something else.*/
 	}
 }
