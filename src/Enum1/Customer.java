c1747a97ea0bc3ee4d0c9f265f239faba95febb4
michmauzer004 <michmauzer004@gmail.com>
2016-06-19 14:06:45 +0300

    initial

--- src/Enum1/Customer.java
+++ src/Enum1/Customer.java
@@ -6,6 +6,13 @@ public class Customer {
 		Customer cust=new Customer();
 		cust.custType=CustomerType.INDIVIDUAL;
 
+		System.out.println(cust);
 		System.out.println(cust.custType);
+
+		Customer cust2=new Customer();
+		cust2.custType=CustomerType.ORGANIZATION;
+		System.out.println(cust2);
+		System.out.println(cust2.custType);
 	}
 }
+//Static finals are not a perfect solution for something that should accept only predefined values.
