03f5e4b4aeedeeec1edff852a0c7e4d1e9f2c1c0
michmauzer004 <michmauzer004@gmail.com>
2016-06-19 15:46:11 +0300

    initial

---
+++ src/EnumInClass/ShapeEnumSwitchTest.java
@@ -0,0 +1,31 @@
+package EnumInClass;
+
+
+public class ShapeEnumSwitchTest {
+
+	public ShapeType shapeType;
+
+	public static void main(String[] args) {
+
+		ShapeEnumSwitchTest shape = new ShapeEnumSwitchTest();
+		shape.shapeType = ShapeType.SQUARE;
+
+		switch (shape.shapeType) {
+			case DOT:
+				System.out.println("This is dot");
+				break;
+			case LINE:
+				System.out.println("This is line");
+				break;
+			case TRIANGLE:
+				System.out.println("This is tr");
+				break;
+			case SQUARE:
+				System.out.println("This is sq");
+				break;
+			default:
+				System.out.println("Unknown value");
+				break;
+		}
+	}
+}
--- src/EnumSwitch/CustomerSwitchTest.java
+++
@@ -1,7 +0,0 @@
-package EnumSwitch;
-
-/**
- * Created by webstddesign on 6/19/2016.
- */
-public class CustomerSwitchTest {
-}
