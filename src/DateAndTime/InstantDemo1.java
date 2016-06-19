bfadecb39a37c3d7d6442c3a350056652ea2a8ed
michmauzer004 <michmauzer004@gmail.com>
2016-06-19 22:46:48 +0300

    initial

--- src/DateAndTime/InstantDemo1.java
+++ src/DateAndTime/InstantDemo1.java
@@ -7,11 +7,14 @@ import java.time.Instant;
 public class InstantDemo1 {
 	public static void main(String[] args) {
 		Instant start = Instant.now();
+		System.out.println(start);
+
 		for (int i=0;i<1000;i++){
 			Integer I=i;
-			System.out.println(I);
+		//	System.out.println(I);
 		}
 		Instant end = Instant.now();
+		System.out.println(end);
 		System.out.println(Duration.between(start, end).toMillis()+" ms");
 	}
 }
