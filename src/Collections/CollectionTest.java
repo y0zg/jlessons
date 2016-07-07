f9545ff463d389f926fa8fed8e13b6bcc66f46c5
michmauzer004 <michmauzer004@gmail.com>
2016-07-07 19:21:20 +0300

    initial

--- src/Collections/CollectionTest.java
+++ src/Collections/CollectionTest.java
@@ -1,7 +1,5 @@
 package Collections;
 
-/**
- * Created by webstddesign on 6/9/2016.
- */
+
 public class CollectionTest {
 }
---
+++ src/Collections/Elephant/Elephant.java
@@ -0,0 +1,19 @@
+package Collections.Elephant;
+
+public class Elephant implements Comparable {
+	public float weight;
+	public int age;
+	public float tuskLength;
+	public int compareTo(Object obj) {
+		Elephant anotherElephant = (Elephant) obj;
+		if (this.weight > anotherElephant.weight) {
+			return 1;
+		} else if (this.weight < anotherElephant.weight) {
+			return -1;
+		} else {
+			// both elephants have the same weight, now
+			// compare their age
+			return (this.age - anotherElephant.age);
+		}
+	}
+}
