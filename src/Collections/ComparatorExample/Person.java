d7c68fbc7fd13e3f17892fc708b3103e525be3f8
michmauzer004 <michmauzer004@gmail.com>
2016-07-10 21:40:36 +0300

    initial

--- src/Collections/ComparatorExample/Person.java
+++ src/Collections/ComparatorExample/Person.java
@@ -22,6 +22,12 @@ public class Person implements Comparable {
 	public void setAge(int age) {
 		this.age = age;
 	}
+//
+//	compare method has the following signature.
+//	public int compare(java.lang.Object o1, java.lang.Object o2)
+//	compare returns zero if o1 and o2 are equal, a negative integer if o1 is less than o2,
+//	and a positive integer if o1 is greater than o2
+
 	public int compareTo(Object anotherPerson)
 			throws ClassCastException {
 		if (!(anotherPerson instanceof Person)) {
