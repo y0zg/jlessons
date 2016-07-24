ab2b8c7d49a10a298b349aea8f184acec95435d6
michmauzer004 <michmauzer004@gmail.com>
2016-07-24 23:36:09 +0300

    initial

--- src/Threads/ThreadDemo1.java
+++ src/Threads/ThreadDemo1.java
@@ -5,7 +5,8 @@ class MyThread extends Thread {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i);
 			try {
-				sleep(100);
+				sleep(200);
+				System.out.println("inside try: "+i);
 			} catch (InterruptedException e) {
 			}
 		}
@@ -14,10 +15,13 @@ public class ThreadDemo1{
 	public static void main(String[] args) {
 		MyThread t1 = new MyThread();
 		MyThread t2 = new MyThread();
-		t1.start();
 		System.out.println(t1.currentThread());
-		t2.start();
 		System.out.println(t2.currentThread());
+		t1.setPriority(1);
+		t2.setPriority(10);
+		t1.start();
+		t2.start();
+
 //		the difference between ThreadDemo0 and ThreadDemo1 classes is
 // that the last one is free to extend another class.
 	}
